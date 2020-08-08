4e804a3540a0188cceae01c7ce7963870bd4415c
Steven Craig Moss <steven-moss@lambdastudents.com>
2020-08-08 16:06:45 -0400

    widget update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-//import Accordion from './components/Accordion';
-import Search from './components/Search'
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 // const items = [
 //     {
 //         title: 'What is React',
@@ -15,10 +16,28 @@ import Search from './components/Search'
 //         content: 'You use React by creating components'
 //     }
 // ]
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label:'The Color Green',
+        value: 'green'
+    },
+    {
+        label:'The Shade of Blue',
+        value:'blue'
+    }
+]
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
     return (
         <div>
-            <Search />
+            <Dropdown 
+            selected={selected} 
+            onSelectedChange={setSelected}
+            options={options}/>
         </div>
     )
 }
