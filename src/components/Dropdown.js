eb177ba6c173cd86c7c735d4fed36b0d10dd8270
Steven Craig Moss <steven-moss@lambdastudents.com>
2020-08-08 21:26:17 -0400

    Translate and Convert components complete

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-const Dropdown = ({options, selected, onSelectedChange }) => {
+const Dropdown = ({ label,options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+    const ref = useRef();
+
+    useEffect(() => {
+        const onBodyClick = (event) => {
+            if(ref.current.contains(event.target)){
+                return;
+            }
+
+            setOpen(false);
+        };
+
+        document.body.addEventListener('click', onBodyClick);
+
+        //this is the cleanup function
+        return () => {
+            document.body.removeEventListener('click', onBodyClick);
+        };
+    }, []);
 
     const renderedOptions = options.map((option) => {
         if(option.value === selected.value) {
@@ -11,18 +29,22 @@ const Dropdown = ({options, selected, onSelectedChange }) => {
             <div 
                 key={option.value} 
                 className='item'
-                onClick={() => onSelectedChange(option)}
-                >
+                onClick={() => {
+                    onSelectedChange(option)
+                }}
+            >
                 {option.label}
             </div>
         );
     });
+    
     return(
-        <div className='ui form'>
+        <div ref={ref} className='ui form'>
             <div className='field'>
-                <label className='label'>Select a Color</label>
+                <label className='label'>{label}</label>
                 <div 
-                    onClick={() => setOpen(!open)}
+                    onClick={() => {
+                        setOpen(!open)}}
                     className={`ui selection dropdown ${open ? 'visible active': ''}`}
                 >
                     <i className='dropdown icon'></i>
